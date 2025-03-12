9d44217a50d5888c35cc5ab4b93f6bae35bbfdba
mmulla6002 <mmulla6002@gmail.com>
2025-03-12 12:28:10 +0000

    Update MAIN_MENU.py

--- MAIN_MENU.py
+++ MAIN_MENU.py
@@ -1,7 +1,25 @@
 from Config import *
 class Main_Menu():
-  def __init__(self):
-    self.active = False
+    def __init__(self):
+        self.active = False
 
-  def run():
-    MAIN.screen.fill((0,128,256))
+    def run():
+        MAIN.screen.fill((0,128,256))
+        if 1P_button.draw(MAIN.screen):
+            self.active = False
+            MAIN.1P_Menu.active = True
+        elif 2P_button.draw(MAIN.screen):
+            self.active = False
+            MAIN.2P_Menu.active = True
+        elif Endless_button.draw(MAIN.screen):
+            self.active = False
+            MAIN.Endless_Menu.active = True
+        elif Quit_button.draw(MAIN.screen):
+            pygame.quit(); sys.exit()
+
+
+1P_buton = button.Button(x,y,width,height)
+2P_button = button.Button(x,y,width,height)
+Endless_button = button.Button(x,y,width,height)
+Quit_button = button.Button(x,y,width,height)
+      
